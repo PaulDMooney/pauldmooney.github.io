6bb4dbf1712d35ec161cc751a09a4a8ef356f3e7
Paul Mooney <pmooney@architech.ca>
2017-10-29 14:29:44 -0400

    More resume data added

--- src/data/resume.js
+++ src/data/resume.js
@@ -58,8 +58,62 @@ export default {
               "Client-side developer consulting and education.",
               "Lead team to develop new site under the DotCMS content management system."
             ]
+          },
+          {
+            name: "Boar’s Head DOCK",
+            description: "Revision of Boar’s Head’s information portal for distributors, named DOCK. Implemented in the DotCMS platform.",
+            responsibilities: [
+              "Planning, estimating, and disseminating work items to team.",
+              "Provided aid for client developers in using DotCMS platform, provided best practice guidelines, development VMs, and jump start for testing in DotCMS.",
+              "Developed extensions for platform to meet client’s custom needs beyond platforms abilities."
+            ]
+          },
+          {
+            name: "Microsoft Technology Center Digital Concierge",
+            description: "A set of web applications to allow client’s of Microsoft’s Technology Center to register for an engagement and check-in on site via facial recognition.",
+            responsibilities: [
+              "Implemented web service layer in C# .Net utilizing Entity Framework 6 and Web API as well as deployments to Azure.",
+              "Investigated walk up detection Javascript libraries to use in client UI.",
+              "Ported facial recognition libraries to web service layer"
+            ]
+          },
+          {
+            name: "CDK Global",
+            description: "Replatforming CDK Global’s dealership software offerings from IBM I-series green screen application to a modern stack implemented using Spring Boot and AngularJS.",
+            responsibilities: [
+              "Provided development team at CDK with guidance on choosing technologies, architecting and implementing in languages and paradigms unfamiliar to them",
+              "Supplemented CDK team’s planning and development efforts"
+            ]
+          },
+          {
+            name: "Serco DriveTest",
+            description: "Large scale software update for DriveTest centers across all of Ontario.",
+            responsibilities: [
+              "Designed interaction between vendor products for scheduling road tests, test center queuing system, and test result tracking systems.",
+              "Verified and validated several use case documents related to bookings, check-ins, and test executions."
+            ]
+          },
+          {
+            name: "Internal",
+            responsibilities: [
+              "Provide pre-sales support including investigations and architecting possible solutions, providing estimates, aiding in development of SOW, client presentations.",
+              "Mentor junior developers in growing their skillsets and proper handling of project duties.",
+              "Promote and create awareness of new technologies and techniques."
+            ]
           }
         ]
+      },
+      {
+        title: "Java EE Developer / Team Lead",
+        company: "QuickPlay Media",
+        from: new Date(2008, 2),
+        to: new Date(2014, 8)
+      },
+      {
+        title: "Java EE/Web Developer",
+        company: "Adjuvant Informatics",
+        from: new Date(2006,1),
+        to: new Date(2008,2),
       }
     ]
   }
